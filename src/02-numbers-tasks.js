c7228802fe6c4e89dabb3b63ca9e3fbbc8c76baf
Aleksandr Ampleev <Aleksandr_Ampleev@epam.com>
2020-07-23 19:17:54 +0400

    add some task(number).

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -184,8 +184,11 @@ function getParallelipidedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const decimal = 10 ** pow;
+  const t = num / decimal;
+  const z = t.toFixed(0) * decimal;
+  return z;
 }
 
 /**
