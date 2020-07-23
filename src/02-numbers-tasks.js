af378c50401bec939320dd4e40d4fef9c1f91c3f
Aleksandr Ampleev <Aleksandr_Ampleev@epam.com>
2020-07-23 17:01:18 +0400

    add some tasks (numbers)

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -111,8 +111,11 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const c = (Math.sqrt(x1 * x1 + y1 * y1) * Math.sqrt(x2 * x2 + y2 * y2));
+  const coss = (x1 * x2 + y1 * y2) / c;
+  const angle = Math.acos(coss);
+  return angle;
 }
 
 /**
