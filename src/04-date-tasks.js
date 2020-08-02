a068b4e72884592ce009aeba2f8ef57b2d08fdbd
Aleksandr Ampleev <Aleksandr_Ampleev@epam.com>
2020-08-03 00:01:08 +0400

    add task (date)

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -93,8 +93,16 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const rez = endDate - startDate;
+  let tmp = rez / 3600000;
+  const h = Math.trunc(tmp % 100).toString().padStart(2, '0');
+  tmp = rez / 60000;
+  const m = Math.trunc(tmp % 60).toString().padStart(2, '0');
+  tmp = rez / 1000;
+  const s = Math.trunc(tmp % 60).toString().padStart(2, '0');
+  const ms = Math.trunc(rez % 1000).toString().padStart(3, '0');
+  return (`${h}:${m}:${s}.${ms}`);
 }
 
 
